7cc2a4f2fc5284dd680e34ad6dc6d40df8fd6127
adzaky15 <adzakymaulana15@gmail.com>
2022-06-30 16:35:33 +0900

    count howl

--- app/src/main/java/com/example/myapplication/FirstFragment.kt
+++ app/src/main/java/com/example/myapplication/FirstFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.myapplication.databinding.FragmentFirstBinding
 
@@ -32,13 +35,49 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
+        // We can either use binding or view for this line
+        // If binding: binding.randomButton.setOnClickListener
+        // If view: view.findViewById<Button>(R.id.random_button).setOnClickListener
+        binding.randomButton.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
-    }
+
+        // Find a howl_button by id
+        view.findViewById<Button>(R.id.howl_button).setOnClickListener {
+            // Create a Toast with some text and duration
+            val myHowl = Toast.makeText(context, "The Black Moon Howls Tonight",
+                Toast.LENGTH_LONG)
+            // Show Toast
+            myHowl.show()
+        }
+
+        // Find a count_button by id
+        view.findViewById<Button>(R.id.count_button).setOnClickListener {
+            // Calls countMe
+            countMe(view)
+        }
+
+
+        }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    // A new function for count_button implementation
+    private fun countMe(view: View) {
+        // Get the text view
+        val showCountTextView = view.findViewById<TextView>(R.id.textview_first)
+
+        // Get the count of text view
+        val countString = showCountTextView.text.toString()
+
+        // Increment count
+        var count = countString.toInt()
+        count++
+
+        // Display new count
+        showCountTextView.text = count.toString()
     }
 }
